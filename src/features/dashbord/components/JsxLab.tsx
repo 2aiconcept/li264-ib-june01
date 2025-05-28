9d9a6816b7c49e35a394fac2e09a5008433620f3
2aiconcept <contact@2aiconcept.com>
2025-05-28 14:22:22 +0200

    Les bases du jsx

--- src/features/dashbord/components/JsxLab.tsx
+++ src/features/dashbord/components/JsxLab.tsx
@@ -3,19 +3,76 @@ export const JsxLab = () => {
   const userName = "Jean Dupont";
   const userAge = 25;
   const isLoggedIn = true;
-  const isAdmin = false;
+  const isAdmin = true;
   let pageTitle = "Laboratoire JSX";
+
+  const changeTitle = () => {
+    pageTitle = "Mon Jsx Lab";
+    console.log(pageTitle);
+  };
+
+  const changeTitleParam = (newTitle: string) => {
+    pageTitle = newTitle;
+    console.log(pageTitle);
+  };
+
+  if (!isLoggedIn) {
+    return (
+      <>
+        <h5>{pageTitle}</h5>
+        <div className="card">
+          <div className="card-header">État de connexion</div>
+          <div className="card-body">
+            <p className="card-text">Vous n'êtes pas connecté.</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <h5>{pageTitle}</h5>
       <div className="card">
-        <div className="card-header">Affichage de variables</div>
+        <div className="card-header">Etat de connexion</div>
         <div className="card-body">
           <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+            Bonjour {userName} vous avez {userAge} ans.
           </p>
+          {/* condition dans du Jsx */}
+          {isAdmin && <p className="card-text">Vous avez les droits admin</p>}
+          {!isAdmin && (
+            <p className="card-text">Vous êtes un utilisateur standard</p>
+          )}
+        </div>
+      </div>
+      <br />
+      <div className="card">
+        <div className="card-header">
+          Etat de connexion (Alternative avec ternaire)
+        </div>
+        <div className="card-body">
+          <p className="card-text">
+            Bonjour {userName} vous avez {userAge} ans.
+          </p>
+          {/* ternaire dans du Jsx */}
+          {isAdmin ? (
+            <p className="card-text">Vous avez les droits admin</p>
+          ) : (
+            <p className="card-text">Vous êtes un utilisateur standard</p>
+          )}
+          <button
+            className="btn btn-primary me-2"
+            onClick={changeTitle}
+          >
+            changer le titre
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeTitleParam("Super Jsx Lab")}
+          >
+            changer le titre avec param string
+          </button>
         </div>
       </div>
     </>
